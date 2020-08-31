2762dbe066ec68b5c2643462aef13c45db1968f1
Martin Petrovaj <martinpetrovaj17@gmail.com>
2020-08-31 13:44:34 +0200

    Add initial database migration

---
+++ Mathesio.Discussion/Mathesio.Discussion.DAL/Migrations/20200831113626_Initial.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mathesio.Discussion.DAL.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(maxLength: 56, nullable: false),
+                    PasswordHash = table.Column<string>(nullable: false),
+                    PasswordSalt = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Threads",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: false),
+                    AuthorID = table.Column<Guid>(nullable: false),
+                    Discriminator = table.Column<string>(nullable: false),
+                    ParentID = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Threads", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Threads_Threads_ParentID",
+                        column: x => x.ParentID,
+                        principalTable: "Threads",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Threads_Authors_AuthorID",
+                        column: x => x.AuthorID,
+                        principalTable: "Authors",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Threads_ParentID",
+                table: "Threads",
+                column: "ParentID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Threads_AuthorID",
+                table: "Threads",
+                column: "AuthorID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Threads");
+
+            migrationBuilder.DropTable(
+                name: "Authors");
+        }
+    }
+}
