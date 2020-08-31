0d7768c1509361efa4ed2b6dea647c4942e15c72
Martin Petrovaj <martinpetrovaj17@gmail.com>
2020-08-31 16:42:19 +0200

    Add missing attribute to Author.ID

--- Mathesio.Discussion/Mathesio.Discussion.DAL/Author.cs
+++ Mathesio.Discussion/Mathesio.Discussion.DAL/Author.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Mathesio.Discussion.DAL
 {
     public class Author
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid ID { get; set; }
 
         [Required]
