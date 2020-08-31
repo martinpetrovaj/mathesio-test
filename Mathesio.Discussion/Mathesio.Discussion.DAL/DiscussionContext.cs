d13a3ec8269a5af71a5a69f687ffeb97a1cb9aa7
Martin Petrovaj <martinpetrovaj17@gmail.com>
2020-08-31 13:44:26 +0200

    Add connection string to appsettings

--- Mathesio.Discussion/Mathesio.Discussion.DAL/DiscussionContext.cs
+++ Mathesio.Discussion/Mathesio.Discussion.DAL/DiscussionContext.cs
@@ -1,16 +1,24 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace Mathesio.Discussion.DAL
 {
     public class DiscussionContext : DbContext
     {
+        private readonly string _connectionString;
+
         public DbSet<Author> Authors { get; set; }
         public DbSet<Thread> Threads { get; set; }
         public DbSet<Comment> Comments { get; set; }
 
+        public DiscussionContext(IConfiguration configuration) : base()
+        {
+            _connectionString = configuration.GetConnectionString("DiscussionDB");
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MathesioDiscussion;Initial Catalog=DiscussionDB;");
+            optionsBuilder.UseSqlServer(_connectionString);
         }
     }
 }
